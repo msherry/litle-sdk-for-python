dbfcbd7867d041cac80628f3bab5ad943640363a
Marc Sherry <msherry@gmail.com>
2013-09-17 18:39:13 -0700

    pep8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='LitleSdkPython',
           'pyxb_114',
       ],
       dependency_links=[
-      'https://github.com/msherry/PyXB-1.1.4/tarball/master#egg=pyxb_114',
+          'https://github.com/msherry/PyXB-1.1.4/tarball/master#egg=pyxb_114',
       ],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
